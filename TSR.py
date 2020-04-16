adc552d3d2ebdc45d6ff215ecd77f661aed2b812
roian6 <roian6@naver.com>
2020-04-16 13:47:56 +0900

    0.1 version

--- TSR.py
+++ TSR.py
@@ -1,3 +1,4 @@
+from apscheduler.triggers.date import DateTrigger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -18,6 +19,7 @@ def start_driver(user_number, user_id, user_pw, article_num):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
+    options.add_argument('log-level=3')
     options.add_argument(
         "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/61.0.3163.100 Safari/537.36")
@@ -39,7 +41,7 @@ def start_driver(user_number, user_id, user_pw, article_num):
     print("학급 게시판에 접속하고 있습니다...")
     driver.refresh()
     driver.get("https://hoc22.ebssw.kr/sunrin206/hmpg/hmpgBbsDetailView.do?menuSn=407600&bbsId=BBSID_000395982"
-               "&bbscttSn="+article_num)
+               "&bbscttSn=" + article_num)
 
     print("댓글을 작성하고 있습니다...")
     input_board_comment = driver.find_element(By.NAME, "cmmntsCn")
@@ -59,18 +61,20 @@ def start_driver(user_number, user_id, user_pw, article_num):
 
     driver.quit()
 
-    print("\n출석이 완료되었습니다!")
+    print("\n출석이 완료되었습니다!\n")
+
+    os.system('pause')
     os._exit(0)
 
 
-print("프로그램 실행 준비중...")
+print("프로그램 실행 준비중...\n")
 board_url = "https://hoc22.ebssw.kr/sunrin206/hmpg/hmpgBbsListView.do?menuSn=407600&bbsId=BBSID_000395982"
 with urllib.request.urlopen(board_url) as response:
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     articles = soup.find_all("a", {"class": "class_nm_ellipsis"})
 
-print("🚀 206 EBS Automation Tool 0.1 by 정찬효 🚀\n")
+print("[ 206 EBS Automation Tool 0.1 by 정찬효 ]\n\n")
 
 if path.exists("user_info.txt"):
     with open('user_info.txt', 'r') as f:
@@ -79,31 +83,32 @@ if path.exists("user_info.txt"):
         student_id = user_info.split(",")[1]
         student_pw = user_info.split(",")[2]
         f.close()
-    print("유저 정보를 불러왔습니다. ("+student_id+")")
+    print("유저 정보를 불러왔습니다! (" + student_id + ")")
 else:
     print("학번을 입력해주세요. (예시: 20625)")
     student_number = input()[-2:]
-    print("EBS ID를 입력해주세요.")
+    print("\nEBS ID를 입력해주세요.")
     student_id = input()
-    print("EBS 패스워드를 입력해주세요.")
+    print("\nEBS 패스워드를 입력해주세요.")
     student_pw = input()
     with open('user_info.txt', 'w') as f:
         f.write(student_number + "," + student_id + "," + student_pw)
-    print("유저 정보가 저장되었으며, user_info.txt 에서 수정할 수 있습니다.")
+    print("\n유저 정보가 저장되었습니다! user_info.txt 에서 수정할 수 있습니다.")
 
-print("출석할 게시물의 번호를 입력해 주세요.")
+print("\n\n출석할 게시물의 번호를 입력해 주세요.\n")
 for i, article in enumerate(articles, start=1):
-    print(str(i)+". "+article.text.strip())
-article_number = articles[int(input())-1].get("href").split("\'")[1]
+    print(str(i) + ". " + article.text.strip())
+article_number = articles[int(input()) - 1].get("href").split("\'")[1]
 
-print("지금 바로 출석할까요? N을 입력하면 출석 시간을 예약할 수 있습니다. (Y,N)")
+print("\n지금 바로 출석할까요? N을 입력하면 출석 시간을 예약할 수 있습니다. (Y,N)")
 now_or_later = input()
 if now_or_later == 'y' or now_or_later == 'Y':
     start_driver(student_number, student_id, student_pw, article_number)
 else:
-    print("예약 시간을 입력해 주세요. (예시: 2020-04-16 06:00:00)")
+    print("\n예약 시간을 입력해 주세요. (예시: 2020-04-16 06:00:00)")
     date_input = input()
-    print(date_input + "에 출석이 진행됩니다.")
+    print(date_input + "에 출석이 진행됩니다. 프로그램을 종료하지 마세요...")
     scheduler = BlockingScheduler()
-    scheduler.add_job(lambda: start_driver(student_number, student_id, student_pw), "date", run_date=date_input)
+    trigger = DateTrigger(run_date=date_input)
+    scheduler.add_job(lambda: start_driver(student_number, student_id, student_pw, article_number), trigger)
     scheduler.start()
---
+++ TSR.spec
@@ -0,0 +1,33 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['TSR.py'],
+             pathex=['D:\\2020개발\\2020코딩\\Selenium206'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='TSR',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=True , icon='C:\\Users\\david0926\\Desktop\\icon.ico')
